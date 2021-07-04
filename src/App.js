1ba48a29db6a69ab15395b01d00e5ac4d50586e7
Ganesh H <ganeshhs2244@gmail.com>
2021-07-04 14:07:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import ExpenseList from "./Component/ExpenseList";
+import ExpenseList from './Component/Expenses/ExpenseList';
+import NewExpense from './Component/NewExpense/NewExpense';
 function App() {
   const expense = [{ Title: "Toilet Paper", Amount: 200, date: new Date(2020, 2, 28) },
       { Title: "Car Insurance", Amount: 500, date: new Date(2020, 2, 20)},
       { Title: "New Table", Amount: 300, date: new Date(2020, 2, 26) }]
   return (
     <div>
+      <NewExpense></NewExpense>
       <ExpenseList data={expense}></ExpenseList>
     </div>
   );
--- src/Component/Expenses/ExpenseList.js
+++ src/Component/Expenses/ExpenseList.js
@@ -1,4 +1,5 @@
 import { data } from "autoprefixer";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import './ExpenseList.css';
 function ExpenseList(props) {
@@ -9,12 +10,12 @@ function ExpenseList(props) {
     const date = new Date(2020, 2, 28);
     console.log(expense);
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem Title={expense[0].Title} Amount={expense[0].Amount} date={expense[0].date}></ExpenseItem>
             <ExpenseItem Title={expense[1].Title} Amount={expense[1].Amount} date={expense[1].date}></ExpenseItem>
             <ExpenseItem Title={expense[2].Title} Amount={expense[2].Amount} date={expense[2].date}></ExpenseItem>
  
-        </div>
+        </Card>
     );
 
 }
